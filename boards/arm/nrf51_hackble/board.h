8488fdbfe92da5cf1c5516c884749ca742294fed
fengmushu <fengmushu@gmail.com>
2017-07-14 10:20:48 +0800

    board nrf51_hackble added

---
+++ boards/arm/nrf51_hackble/board.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2016 Linaro Limited.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+/* Push button switch 0 */
+#define SW0_GPIO_PIN	17
+#define SW0_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+#endif /* __INC_BOARD_H */
